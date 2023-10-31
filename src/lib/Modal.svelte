9dc367833b8ae4ef156d602efffde71a28733ec6
Luke Harby <Luke.Harby@akqa.com>
2023-10-31 12:38:42 +0000

    Slot props (and added chapters)

---
+++ src/lib/Modal.svelte
@@ -0,0 +1,46 @@
+<script>
+	let hover = false;
+</script>
+
+<div class="modal p-4 rounded-md" on:click|self on:keydown role="presentation">
+	{#if $$slots.title}
+		<slot name="title"
+			><h1 class="font-bold p-4 bg-zinc-900 text-white"><!-- optional fallback --></h1></slot
+		>
+	{/if}
+	<slot name="content">
+		<p>No content provided</p>
+	</slot>
+	<div
+		role="presentation"
+		on:mouseenter={() => {
+			hover = true;
+		}}
+		on:mouseleave={() => {
+			hover = false;
+		}}
+	>
+		<slot name="cta" {hover}>
+			<button on:click|self>Close modal</button>
+		</slot>
+	</div>
+</div>
+<div class="modal-backdrop" />
+
+<style>
+	.modal {
+		position: relative;
+		margin: 2rem;
+		background-color: #fff;
+		z-index: 2;
+	}
+	.modal-backdrop {
+		position: fixed;
+		background-color: rgba(0, 0, 0, 0.7);
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 1;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script>
-	import '../app.css';
-	import ShoppingCart from '$lib/ShoppingCart.svelte';
 	import CartItemsStore from '../Store.js';
+	import ShoppingCart from '$lib/ShoppingCart.svelte';
+	import Modal from '$lib/Modal.svelte';
+	import '../app.css';
 
 	let showCart = false;
 	let cartCount = 0;
@@ -16,9 +17,29 @@
 	function addItemToCart(event) {
 		cartItems = [...cartItems, event.detail.item];
 	}
+
+	let showModal = true;
 </script>
 
 <div>
+	{#if showModal}
+		<Modal on:click={() => (showModal = false)}>
+			<div slot="title">Modal title</div>
+			<div slot="content">20% off in December, hurry</div>
+			<div slot="cta">
+				<button on:click={() => (showModal = false)}>OK</button>
+			</div>
+		</Modal>
+
+		<Modal on:click={() => (showModal = false)}>
+			<div slot="content">20% off in December, hurry</div>
+			<div slot="cta" let:hover={hovering}>
+				<button on:click={() => (showModal = false)}
+					>{#if hovering}Are you sure you want to close? {:else} OK{/if}</button
+				>
+			</div>
+		</Modal>
+	{/if}
 	<div>
 		<nav class="flex items-center border-b border-zinc-700 p-4 lg:px-6 bg-black">
 			<div class="flex w-full items-center justify-between">
